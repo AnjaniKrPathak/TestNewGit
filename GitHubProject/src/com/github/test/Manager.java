0aa4fab1993597af119b88fb2cba284c7fd3ae94
IDC-0004 <IDC-0004@IDC-0004-PC>
2016-03-08 14:10:10 +0530

    Nagraj

--- GitHubProject/src/com/github/test/Manager.java
+++ GitHubProject/src/com/github/test/Manager.java
@@ -2,7 +2,7 @@ package com.github.test;
 
 public class Manager {
 	public static void main(String[] args) {
-		System.out.println("Hi Github ");
+		System.out.println("Hi Github Nagraj ");
 	}
 
 }
